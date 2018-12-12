9f4a85210d24a835c171cebcfe523f4a504323e6
indrajit <indrashanbhag@gmail.com>
2018-12-12 07:31:43 -0500

    testing changes

--- show_me_the_money/lib/show_me_the_money_web/controllers/page_controller.ex
+++ show_me_the_money/lib/show_me_the_money_web/controllers/page_controller.ex
@@ -2,6 +2,18 @@ defmodule ShowMeTheMoneyWeb.PageController do
   use ShowMeTheMoneyWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    {:ok, simulator} = CryptoCoin.Simulator.start_link(nil)
+    CryptoCoin.Simulator.set_state_change_listener(simulator, self())
+    render(conn, "index.html", simulator: simulator)
   end
+
+# Does not work
+  # def handle_info(
+  #       {:network_state, chain_length, number_of_nodes, number_of_wallets, wallets_amount_map,
+  #        block_chain_length_time_map, block_chain_length_nounce_value_map},
+  #       state
+  #     ) do
+  #   IO.puts("got the state change thingy")
+  #   {:noreply, state}
+  # end
 end
