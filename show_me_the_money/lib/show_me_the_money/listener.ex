5b53be7d5dabd8b27388ed0594650ef20f460f24
indrajit <indrashanbhag@gmail.com>
2018-12-14 04:15:03 +0530

    Listener timer related chnages

--- show_me_the_money/lib/show_me_the_money/listener.ex
+++ show_me_the_money/lib/show_me_the_money/listener.ex
@@ -9,13 +9,25 @@ defmodule CryptoCoin.Listener do
   end
 
   def handle_cast({:start_listening, simulator}, state) do
-    spawn(start_listening_simulator(simulator, 50))
+    send(self(), {:query_simlator_state, simulator})
+    state = state |> Map.put(:simulator, simulator)
+    {:noreply, state |> Map.put(:query_count, 0)}
+  end
+
+  def handle_info({:query_simlator_state, simulator}, state) do
+    CryptoCoin.Simulator.get_simulator_stats(simulator)
+
+    count = state.query_count
+    if (count < 50) do
+      Process.send_after(self(), {:query_simlator_state, simulator}, 50)
+    end
+
     {:noreply, state}
   end
 
-  def handle_cast({:network_state, chain_length, num_nodes, num_wallets, wallets_amount_map, block_chain_length_time_map, block_chain_length_nounce_value_map}, state) do
+  def handle_info({:network_state, chain_length, num_nodes, num_wallets, wallets_amount_map, block_chain_length_time_map, block_chain_length_nounce_value_map}, state) do
     IO.puts "chain_length = #{chain_length}"
-    {:noreply, state}
+    {:noreply, state |> Map.put(:query_count, state.query_count + 1)}
   end
 
   # def handle_cast({:network_state, chain_length}, state) do
@@ -28,13 +40,13 @@ defmodule CryptoCoin.Listener do
     {:ok, state}
   end
 
-  defp start_listening_simulator(_pid, count) when count < 1 do
-    nil
-  end
-
-  defp start_listening_simulator(pid, count) do
-    CryptoCoin.Simulator.get_simulator_stats(pid)
-    :timer.sleep(5000)
-    start_listening_simulator(pid, count - 1)
-  end
+  # defp start_listening_simulator(_pid, count) when count < 1 do
+  #   nil
+  # end
+  #
+  # defp start_listening_simulator(pid, count) do
+  #   CryptoCoin.Simulator.get_simulator_stats(pid)
+  #   :timer.sleep(5000)
+  #   start_listening_simulator(pid, count - 1)
+  # end
 end
